49bd0b1113459357195fa50eb7fa727a951de7eb
Flavio De Stefano <destefano.flavio@gmail.com>
2024-08-10 11:14:32 +0200

    Fix cancelled state

--- SLWidget.js
+++ SLWidget.js
@@ -1,11 +1,5 @@
 module.exports.run = async ({ SITE_ID, TRANSPORT, LINE, DIRECTION }) => {
-  // Used only to refresh widget
-  if (args.queryParameters.refresh) {
-    App.close();
-    return;
-  }
-
-  function getIconByTransport() {
+  function getIconForTransport() {
     switch (TRANSPORT) {
       case "BUS":
         return "🚌";
@@ -33,68 +27,82 @@ module.exports.run = async ({ SITE_ID, TRANSPORT, LINE, DIRECTION }) => {
     return json;
   }
 
-  const data = await loadData();
-  const first = data.departures[0];
+  // Used only to refresh widget
+  if (args.queryParameters.refresh) {
+    App.close();
+    return;
+  }
 
   const widget = new ListWidget();
   widget.useDefaultPadding();
-  widget.refreshAfterDate = new Date(first.expected);
-
   widget.backgroundColor = new Color("#44464C");
 
   const viewStack = widget.addStack();
   viewStack.layoutVertically();
   viewStack.centerAlignContent();
 
-  const departure = viewStack.addText(
-    `${getIconByTransport(TRANSPORT)} ${first.stop_area.name}`
-  );
-  departure.font = Font.mediumSystemFont(12);
-  departure.textColor = new Color("#FFFFFF");
+  const data = await loadData();
+  const departures = data.departures.filter((e) => e.state !== "CANCELLED");
 
-  const destination = viewStack.addText(
-    `${first.line.designation} to ${first.destination}`
-  );
-  destination.font = Font.mediumSystemFont(12);
-  destination.textColor = new Color("#FFFFFF");
+  if (departures.length === 0) {
+    const error = viewStack.addText("No departures found");
+    error.font = Font.mediumSystemFont(22);
+    error.textColor = new Color("#FFFFFF");
+  } else {
+    const first = departures[0];
 
-  viewStack.addSpacer(4);
+    widget.refreshAfterDate = new Date(first.expected);
 
-  let i = 0;
-  for (const d of data.departures) {
-    const display = viewStack.addDate(new Date(d.expected));
-    display.font = Font.boldSystemFont(38 - i * i * 16);
-    display.textColor = new Color("#FFFFFF");
-    display.applyTimeStyle();
+    const departure = viewStack.addText(
+      `${getIconForTransport(TRANSPORT)} ${first.stop_area.name}`
+    );
+    departure.font = Font.mediumSystemFont(12);
+    departure.textColor = new Color("#FFFFFF");
 
-    const expected = new Date(d.expected) / (1000 * 60);
-    const scheduled = new Date(d.scheduled) / (1000 * 60);
+    const destination = viewStack.addText(
+      `${first.line.designation} to ${first.destination}`
+    );
+    destination.font = Font.mediumSystemFont(12);
+    destination.textColor = new Color("#FFFFFF");
 
-    if (expected - scheduled > 2) {
-      display.textColor = new Color("#FF0000");
-    } else if (expected - scheduled > 1) {
-      display.textColor = new Color("#FFFF00");
-    } else {
-      display.textColor = new Color("#00FF00");
+    viewStack.addSpacer(4);
+
+    let i = 0;
+    for (const d of departures) {
+      const display = viewStack.addDate(new Date(d.expected));
+      display.font = Font.boldSystemFont(38 - i * i * 16);
+      display.textColor = new Color("#FFFFFF");
+      display.applyTimeStyle();
+
+      const expected = new Date(d.expected) / (1000 * 60);
+      const scheduled = new Date(d.scheduled) / (1000 * 60);
+
+      if (expected - scheduled > 2) {
+        display.textColor = new Color("#FF0000");
+      } else if (expected - scheduled > 1) {
+        display.textColor = new Color("#FFFF00");
+      } else {
+        display.textColor = new Color("#00FF00");
+      }
+
+      if (++i >= 2) break;
     }
 
-    if (++i >= 2) break;
+    viewStack.addSpacer(8);
+
+    // Add last updated to
+    const hStack = widget.addStack();
+    hStack.layoutHorizontally();
+
+    const label = hStack.addText("⏳ ");
+    label.font = Font.mediumSystemFont(8);
+    label.textColor = new Color("#FFFFFF");
+
+    const update = hStack.addDate(new Date());
+    update.applyRelativeStyle();
+    update.font = Font.mediumSystemFont(8);
+    update.textColor = new Color("#FFFFFF");
   }
-
-  viewStack.addSpacer(8);
-
-  // Add last updated to
-  const hStack = widget.addStack();
-  hStack.layoutHorizontally();
-
-  const label = hStack.addText("⏳ ");
-  label.font = Font.mediumSystemFont(8);
-  label.textColor = new Color("#FFFFFF");
-
-  const update = hStack.addDate(new Date());
-  update.applyRelativeStyle();
-  update.font = Font.mediumSystemFont(8);
-  update.textColor = new Color("#FFFFFF");
 
   // Override what the script does
   widget.url = `scriptable:///run/${encodeURIComponent(
